55c9e3d60640130776e4d49524cef71f40074d7f
SamirFarhat17 <samirfarhat17@gmail.com>
2022-03-01 14:40:59 -0500

    Formatted consistently

--- HW2/KMP.cpp
+++ HW2/KMP.cpp
@@ -1,17 +1,16 @@
 
-// C++ program for implementation of KMP pattern searching 
-// algorithm 
+// C++ program for implementation of KMP pattern searching
+// algorithm
 #include <iostream>
 using namespace std;
 
 #include "KMP.h"
 
-  
-// Driver program to test above function 
-int main() 
-{ 
-    string txt = "AAACAAAAACAAACAAAAAC"; 
-    string pat = "AAA"; 
-    KMPSearch(pat, txt); 
-    return 0; 
-} 
+
+// Driver program to test above function
+int main() {
+    string txt = "AAACAAAAACAAACAAAAAC";
+    string pat = "AAA";
+    KMPSearch(pat, txt);
+    return 0;
+}
