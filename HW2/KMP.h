7a4703545ca9651f2722306a083cdf9771a84fa4
SamirFarhat17 <samirfarhat17@gmail.com>
2022-03-03 18:39:25 -0500

    HW2: All Done!

--- HW2/KMP.h
+++ HW2/KMP.h
@@ -4,23 +4,21 @@
 // Fills lps[] for given patttern pat[0..M-1]
 void computePiArray(string pat, int M, int* pi) {
     //Finish this function
-    int len = 0;
+    int k = 0;
     int i = 1;
     pi[0] = 0;
 
     while(i < M) {
-        if(pat[i] == pat[len]) {
-            len++;
-            pi[i] = len;
+        if(pat[i] == pat[k]) {
+            k++;
+            pi[i] = k;
             i++;
         }
-        else {
-            if(len != 0) len = pi[len-1];
-            else if(len == 0) {
+        else if(k == 0) {
                 pi[i] = 0;
                 i++;
             }
-        }
+        else k = pi[k-1];
     }
 }
 
@@ -32,21 +30,21 @@ void KMPSearch(string pat, string txt) {
     // Preprocess the pattern (calculate lps[] array)
     computePiArray(pat, M, pi);
 
-    int i = 0;
     int j = 0;
+    int k = 0;
 
-    while(i < N) {
-        if(txt[i] == pat[j]) {
-            i++;
+    while(j < N) {
+        if(txt[j] == pat[k]) {
             j++;
+            k++;
         }
-        if(j == M) {
-            std::cout << "Found pattern at index " << i - j << endl;
-            j = pi[j-1];
+        if(k == M) {
+            std::cout << "Found pattern at index " << j - k << endl;
+            k = pi[k-1];
         }
-        else if(i < N && pat[j] != txt[i]) {
-            if(j != 0) j = pi[j - 1];
-            else i =i+1;
+        else if(j < N && pat[k] != txt[j]) {
+            if(k != 0) k = pi[k - 1];
+            else j++;
         }
     }
 }
